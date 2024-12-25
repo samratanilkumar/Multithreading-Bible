369f6e8b71c5a83e72bf238c05060353b02f102d
samratanilkumar <samratanilkumar5@gmail.com>
2024-12-25 16:20:00 +0530

    revised hello_world multithreading

--- ThreadBasics/HelloWorld/hello_world.c
+++ ThreadBasics/HelloWorld/hello_world.c
@@ -25,50 +25,37 @@
 /* A thread callback fn must have following prototypes 
  * void *(*thread_fn)(void *)
  * */
-static void *
-thread_fn_callback(void *arg) {
 
-	char *input = (char *)arg;
-
-	while(1) {	
-		printf("input string = %s\n", input);
+static void* thread_fn_callback(void*arg){
+	char* input=(char*)arg;
+	while(1){
+		printf( " the input string is : %s\n",input);
 		sleep(1);
 	}
 }
+void thread_create(){
 
-void
-thread1_create() {
-
-	/* opaque object, dont bother about its internal
-	 * members */
-	pthread_t pthread1;
-
-	/* Take some argument to be passed to the thread fn,
- 	 * Look after that you always paas the persistent memory
- 	 * as an argument to the thread, do not pass caller's 
- 	 * local variables Or stack Memory*/	
-	static char *thread_input1 = "I am thread no 1";
-
-	/* Return 0 on success, otherwise returns errorcode, all
- 	 * pthread functions return -ve error code on failure, they
- 	 * do not set global 'errno' variable */
-	int rc = pthread_create(&pthread1, 
-				   NULL, 
-				   thread_fn_callback,
-				   (void *)thread_input1);
-	if(rc != 0) {
-
-		printf("Error occurred, thread could not be created, errno = %d\n", rc);
-		exit(0);
+	pthread_t thread1;
+	static char *threadinput1 = "hello im samrat " ;
+	int rc = pthread_create (&thread1, NULL, thread_fn_callback, (void*)threadinput1);
+	if(rc!=0){
+		printf("error\n");
 	}
+
 }
 
-int
-main(int argc, char **argv){
 
-	thread1_create();
-	printf("main fn paused\n");
-	pause();
-	return 0;
+
+
+int main (int argc, char**argv){
+	thread_create();
+	// now pause 
+	printf("main thread is paused \n ");
+	while(1){
+		printf("main thread is runing... \n");
+		sleep(1);
+	}
+	pause(); 
+  return 0 ;
+
 }
-
